c5615b5898441b4e34b1a62d4a85e5c9180c9019
sarah-lima <sarah.lima@bandtec.com.br>
2022-08-30 02:01:08 -0300

    projeto layout finalizado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
 import { Component } from '@angular/core';
+import api from 'src/api/index';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'challenge-itidigital-frontend';
+  password = 'assets/password.png';
+  passwordValue: string = '';
+  result:any = '';
+  color:any = '';
+
+  passwordChange(newValue: any) {
+    this.passwordValue = newValue.target.value;
+  }
+  validatePassword(value: string) {
+    api
+      .post('', {
+        password: value,
+      })
+      .then(({ data }) => {
+        if(data) {
+          this.result = 'Senha válida';
+          this.color = '#41D461';
+        }}
+      )
+      .catch((error)=>{
+        if(error.response.status === 401){
+          this.result = 'Senha inválida';
+          this.color = '#D44141';
+        }
+        console.log(error)
+      })
+    console.log(value);
+    console.log(this.result);
+  }
 }
